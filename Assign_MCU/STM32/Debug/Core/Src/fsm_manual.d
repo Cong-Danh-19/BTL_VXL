2670d777fd51bddd6de452c4b0218b61d5e257e4
Cong-Danh-19 <danh.hocongdanh@hcmut.edu.vn>
2025-12-12 13:43:16 +0700

    Cap nhat code moi nhat

--- Assign_MCU/STM32/Debug/Core/Src/fsm_manual.d
+++ Assign_MCU/STM32/Debug/Core/Src/fsm_manual.d
@@ -26,7 +26,7 @@ Core/Src/fsm_manual.o: ../Core/Src/fsm_manual.c ../Core/Inc/fsm_manual.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
  ../Core/Inc/software_timer.h ../Core/Inc/led_traffic.h \
- ../Core/Inc/global.h ../Core/Inc/lcd_i2c.h
+ ../Core/Inc/global.h ../Core/Inc/lcd_i2c.h ../Core/Inc/fsm_automatic.h
 
 ../Core/Inc/fsm_manual.h:
 
@@ -91,3 +91,5 @@ Core/Src/fsm_manual.o: ../Core/Src/fsm_manual.c ../Core/Inc/fsm_manual.h \
 ../Core/Inc/global.h:
 
 ../Core/Inc/lcd_i2c.h:
+
+../Core/Inc/fsm_automatic.h:
